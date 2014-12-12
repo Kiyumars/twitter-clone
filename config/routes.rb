1299621624c6e5130b4ea4bddc26e7875e3005dd
Kiyu <qconnection@gmail.com>
2014-12-12 19:47:16 +0100

    Add layouts and integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
