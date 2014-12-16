f9a609d236b76353f3377a6bb472399e03d93ed7
Kiyu <qconnection@gmail.com>
2014-12-16 19:39:29 +0330

    Complete Create action of Session controller, fix flash-danger issue

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		#Log user in and redirect to user index
+  	else
+  		flash.now[:danger] = 'Invalid email/password combination'
+	  	render 'new'
+	  end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root "static_pages#home"
 
@@ -7,6 +6,10 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+
+  get 'login' 	=> 'sessions#new'
+  post 'login'	=> 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
 
