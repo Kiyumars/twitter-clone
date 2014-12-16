aa0e354ce2a3ae38ed55ce70130bb16ef49632ae
Kiyu <qconnection@gmail.com>
2014-12-16 21:46:30 +0330

    Complete logout functionality

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,10 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  #Returns true if test_user is logged in
+  def is_logged_in?
+  	!session[:user_id].nil?
+  end
+
   # Add more helper methods to be used by all tests here...
 end
