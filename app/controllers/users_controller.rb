1f71abf3f49d5b95fd2eb6582118fb90e6478d59
Kiyu <qconnection@gmail.com>
2014-12-17 22:03:58 +0330

    Add index page to list all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [ :index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
-	def show
+	def index
+    @users = User.all
+  end
+
+  def show
     @user = User.find(params[:id])
   end
 
