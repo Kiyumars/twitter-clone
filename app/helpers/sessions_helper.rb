63413e0f99b479cb68339c8ecaec4ec243db2543
Kiyu <qconnection@gmail.com>
2014-12-17 21:47:48 +0330

    added friendly forwarding after login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,4 +42,13 @@ module SessionsHelper
   def current_user?(user)
   	user == current_user
   end
+
+  def redirect_back_or(default)
+  	redirect_to(session[:forwarding_url] || default)
+  	session.delete(:forwarding_url)
+  end
+
+  def store_location
+  	session[:forwarding_url] = request.url if request.get?
+  end
 end
